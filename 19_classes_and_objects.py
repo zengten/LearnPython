7ee53336be8615a4682d5e047c27342f7c81599a
teng4j <zengten@126.com>
2023-06-16 23:29:38 +0800

    feat 类,对象,继承使用

---
+++ 19_classes_and_objects.py
@@ -0,0 +1,72 @@
+# 定义class
+class Person:
+    def __init__(self):
+        self.nickname = 'ZhangSan'
+
+
+# 实例化对象
+person = Person()
+print(person)  # <__main__.Person object at 0x0000021945829CA0>
+print(person.nickname)  # ZhangSan
+
+
+class User:
+    # 构造器指定默认值，相当于多种构造期
+    def __init__(self, nickname='', age=0, gender='保密'):
+        self.nickname = nickname
+        self.age = age
+        self.gender = gender
+        self.skills = []
+
+    # 默认方法是实例方法,也可以指定@classmethod表示实例方法
+    def user_info(self):
+        print(f'nickname={self.nickname},age={self.age},gender={self.gender},skills={self.skills}')
+
+    # 表示静态方法
+    @staticmethod
+    def say_hello(args1, args2):
+        print(f'hello static method, args1={args1}, args2={args2}')
+
+    def add_skill(self, args):
+        self.skills.append(args)
+
+
+User.say_hello('hello', 'world')  # hello static method, args1=hello, args2=world
+user = User()
+user.user_info()  # nickname=,age=0,gender=保密
+user.add_skill('Java')
+user.add_skill('Python')
+user.user_info()  # nickname=,age=0,gender=保密,skills=['Java', 'Python']
+
+
+# 继承
+class Animal:  # 动物父类
+    def __init__(self, name):
+        self.name = name
+
+    def make_sounds(self):
+        pass
+
+
+class Dog(Animal):  # 狗继承动物
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def make_sounds(self):
+        print(f'{self.name},age={self.age} 汪汪汪~')
+
+
+class Cat(Animal):  # 狗继承动物
+    def __init__(self, name, gender):
+        self.gender = gender
+        super().__init__(name)
+
+    def make_sounds(self):
+        print(f'{self.name},gender={self.gender} 喵喵喵~')
+
+
+dog = Dog('小七', 22)
+dog.make_sounds()  # 小七,age=22 汪汪汪~
+cat = Cat('苗苗', '男')
+cat.make_sounds()  # 苗苗,gender=男 喵喵喵~
